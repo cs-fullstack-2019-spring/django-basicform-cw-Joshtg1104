9acb19d9bad8617de61c1c02650a394451c8d785
Joshtg1104 <joshtg1104@gmail.com>
2019-02-25 17:03:36 -0600

    AAAAARGH, I wish I could've fixed this

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("helloMate/", views.helloMate, name="helloMate"),
+    path("add5Dollars/<int:accountID>/", views.add5Dollars, name="add5Dollars"),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test URL")
+    accounts = Account.objects.all()
+    context = {
+        'accounts': accounts
+    }
+    return render(request, 'accountApp/index.html', context)
+
+
+def helloMate(request):
+    print(request.POST)
+    print(request.POST["personName"])
+    return HttpResponse("Welcome to " + request.POST["personName"] + "'s Account")
+
+
+def add5Dollars(request, accountID):
+    separateAccount = get_object_or_404(Account, pk=accountID)
+    separateAccount.checking += 5
+    separateAccount.save()
+
+    accounts = Account.objects.all()
+
+    context = {
+        "accounts": accounts
+    }
+
+    return render(request, 'accountApp/index.html', context)
